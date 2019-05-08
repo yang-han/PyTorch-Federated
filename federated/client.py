a6ece2d8fa330be3bb85b83694620aa4c9b60d36
Han YANG <yanghan9826@gmail.com>
2019-05-08 11:41:43 +0800

    Update

--- federated/client.py
+++ federated/client.py
@@ -1,10 +1,19 @@
 import random
 
+
 class Client(object):
-    def __init__(self, *args):
-        pass
-    def train(self):
-        pass
+    def __init__(self, client_id, model, train_data, test_data):
+        self.id = client_id
+        self.model = model
+        self.train_data = train_data
+        self.test_data = test_data
+
+    # TODO: Finish this method
+    def train(self, epochs, batch_size):
+        for i in range(epochs):
+            update = self.model.train(train_data)
+        num_train_samples = len(self.train_data)
+        return num_train_samples, update
 
 
 
@@ -12,11 +21,10 @@ class Server(object):
     def __init__(self, clients, model):
         self.clients = clients
         self.model = model
-        
+
     def select_clients(self, possible_clients, num_clients):
         num_clients = min(num_clients, len(possible_clients))
         self.selected_clients = random.sample(possible_clients, num_clients)
-
 
     @property
     def online(self):
@@ -24,8 +32,3 @@ class Server(object):
 
     def train(self):
         pass
-
-
-
-
-    
